f80d1ee4284477147c6056bf5dadd3d5811809ab
Avroumi <avroumi97@gmail.com>
2026-06-23 15:52:22 +0300

    1-5

---
+++ stringMethod.js
@@ -0,0 +1,11 @@
+const str1 = " hello world "
+console.log(str1.trim())
+
+const str2 = "avroumi97@gmail.com"
+console.log(str2.includes("@"))
+
+console.log(str1.toUpperCase())
+
+const str3 = "HELLO WORLD"
+console.log(str3.toLowerCase())
+
