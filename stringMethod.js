ce372e66ed4279891796cbe7a37f250017e8ef54
Avroumi <avroumi97@gmail.com>
2026-06-23 16:20:43 +0300

    finish teh stringMethod

--- stringMethod.js
+++ stringMethod.js
@@ -20,3 +20,29 @@ console.log(str5.startsWith("https"))
 console.log(str5.endsWith(".com"))
 
 console.log(str1.replace("hello", "hi"))
+
+const str6 = "banana"
+console.log(str6.replaceAll("a", "o"))
+
+const str7 = "one two three"
+console.log(str7.split(" "))
+
+console.log(str7.split(" ", 2))
+
+console.log(str6.indexOf("a"))
+
+console.log(str6.lastIndexOf("a"))
+
+
+
+const str8 = "7"
+console.log(str8.padStart(3, "0"))
+
+const str9 = "hi"
+console.log(str9.padEnd(5, "*"))
+
+console.log(str1.charAt(1))
+
+console.log(str1.at(-5))
+
+console.log(str4.substring(2, 6))
