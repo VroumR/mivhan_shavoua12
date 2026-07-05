238b755d5dca342592ae16b2c5006674d3c8a2f6
Avroumi <avroumi97@gmail.com>
2026-07-05 12:37:42 +0300

    FIX : data.js

--- shavouaTwoJS/mivhanShavoua12/data.js
+++ shavouaTwoJS/mivhanShavoua12/data.js
@@ -3,6 +3,7 @@ import fs from "fs/promises"
 const linkFile = "https://server-for-test-week-13.onrender.com/api/race"
 
 export const catchData = async () => {
+    try{
     const res = await fetch(linkFile)
     if (!res.ok){
         return res.status
@@ -10,12 +11,18 @@ export const catchData = async () => {
     const data = await res.json()
     await fs.writeFile("./course-info.js", JSON.stringify(data, null , 2), "utf-8")
     return  data
+    
+}catch(err){
+    throw new Error("file Not found")
 }
+};
+
+
 
 export const readFile = async () => {
     const data = await fs.readFile("./course-info.js", "utf-8")
-    return await JSON.parse(data)
+    const readble =   await JSON.parse(data)
+    return readble
 }
-
 
 
